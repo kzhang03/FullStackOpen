4f90dec2ee1826e04f861f84058e9337f9e13eba
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-01 18:08:56 -0400

    Completed 1.6

--- Part 1/src/App.jsx
+++ Part 1/src/App.jsx
@@ -1,25 +1,25 @@
 import { useState } from 'react'
 
-const Header = ( {name} ) => {
+const Title = ({ title }) => {
   return (
-    <div>
-      <h1><b>{name}</b></h1>
-    </div>
+    <h1>
+      {title}
+    </h1>
   )
 }
 
-const Button = ( {name, handleClick} ) => {
+const Button = ({ title, handler}) => {
   return (
-    <div>
-      <button onClick={handleClick}>{name}</button>
-    </div>
+    <button onClick={handler}>
+      {title} 
+    </button>
   )
 }
 
-const DisplayFeedback = ( {alignment, counter} ) => {
+const Statistic = ({ name,val}) => {
   return (
     <div>
-      <pre>{alignment} {counter}</pre>
+      {name} {val}
     </div>
   )
 }
@@ -29,39 +29,29 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
 
-  const handleGood = () => {
-    setGood(good => good + 1)
-    setAll(all => all + 1)
+  const increaseGood = () => {
+    setGood(good + 1)
   }
 
-  const handleNeutral = () => {
-    setNeutral(neutral => neutral + 1)
-    setAll(all => all + 1)
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
   }
 
-  const handleBad = () => {
-    setBad(bad => bad + 1)
-    setAll(all => all + 1)
+  const increaseBad = () => {
+    setBad(bad + 1)
   }
 
-  const average = all !== 0 ? (good - bad) / all : 0; 
-  const positivePercentage = all !== 0 ? (good / all) * 100 : 0;
-  
   return (
     <div>
-      <Header name="give feedback" />
-      <Button handleClick={handleGood} name={"good"} />
-      <Button handleClick={handleNeutral} name={"neutral"} />
-      <Button handleClick={handleBad} name={"bad"} />
-      <Header name="statistics" />
-      <DisplayFeedback alignment={"good"} counter={good} />
-      <DisplayFeedback alignment={"neutral"} counter={neutral} />
-      <DisplayFeedback alignment={"bad"} counter={bad} />
-      <DisplayFeedback alignment={"all"} counter={all} />
-      <DisplayFeedback alignment={"average"} counter={average} />
-      <DisplayFeedback alignment={"positive"} counter={positivePercentage + " %"} />
+      <Title title="give feedback" />
+      <Button title="good" handler={increaseGood}/>
+      <Button title="neutral" handler={increaseNeutral}/>
+      <Button title="bad" handler={increaseBad}/>
+      <Title title="statistics" />
+      <Statistic name="good" val={good} />
+      <Statistic name="neutral" val={neutral} />
+      <Statistic name="bad" val={bad} />
     </div>
   )
 }
