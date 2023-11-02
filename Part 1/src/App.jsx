1a1198bcc59b0f8065f3280cccb36662f5d406cb
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-01 22:44:12 -0400

    Finished

--- Part 1/src/App.jsx
+++ Part 1/src/App.jsx
@@ -1,67 +1,40 @@
 import { useState } from 'react'
 
-const Header = ( {name} ) => {
-  return (
-    <div>
-      <h1><b>{name}</b></h1>
-    </div>
-  )
-}
-
-const Button = ( {name, handleClick} ) => {
-  return (
-    <div>
-      <button onClick={handleClick}>{name}</button>
-    </div>
-  )
-}
-
-const DisplayFeedback = ( {alignment, counter} ) => {
-  return (
-    <div>
-      <pre>{alignment} {counter}</pre>
-    </div>
-  )
+const Button = ({ name, handler }) => {
+  return <button onClick={handler}>{name}</button>
 }
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(8).fill(0))
 
-  const handleGood = () => {
-    setGood(good => good + 1)
-    setAll(all => all + 1)
+  const nextAnecdote = () => {
+    setSelected((selected + 1) % anecdotes.length)
   }
 
-  const handleNeutral = () => {
-    setNeutral(neutral => neutral + 1)
-    setAll(all => all + 1)
+  const increaseVote = () => {
+    const newVotes = [...votes]
+    newVotes[selected] = newVotes[selected] + 1
+    setVotes(newVotes)
   }
 
-  const handleBad = () => {
-    setBad(bad => bad + 1)
-    setAll(all => all + 1)
-  }
-
-  const average = all !== 0 ? (good - bad) / all : 0; 
-  const positivePercentage = all !== 0 ? (good / all) * 100 : 0;
-  
   return (
     <div>
-      <Header name="give feedback" />
-      <Button handleClick={handleGood} name={"good"} />
-      <Button handleClick={handleNeutral} name={"neutral"} />
-      <Button handleClick={handleBad} name={"bad"} />
-      <Header name="statistics" />
-      <DisplayFeedback alignment={"good"} counter={good} />
-      <DisplayFeedback alignment={"neutral"} counter={neutral} />
-      <DisplayFeedback alignment={"bad"} counter={bad} />
-      <DisplayFeedback alignment={"all"} counter={all} />
-      <DisplayFeedback alignment={"average"} counter={average} />
-      <DisplayFeedback alignment={"positive"} counter={positivePercentage + " %"} />
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <Button name="vote" handler={increaseVote} />
+      <Button name="next anecdote" handler={nextAnecdote} />
     </div>
   )
 }
