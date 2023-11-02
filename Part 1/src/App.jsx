4e7e0c32d83c57771fe527bf008f116b59f0e56e
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-01 21:47:52 -0400

    Finished it

--- Part 1/src/App.jsx
+++ Part 1/src/App.jsx
@@ -1,78 +1,69 @@
 import { useState } from 'react'
 
 const Title = ({ title }) => {
-  return (
-    <h1>
-      {title}
-    </h1>
-  )
+  return <h1>{title}</h1>
 }
 
-const Button = ({ title, handler}) => {
-  return (
-    <button onClick={handler}>
-      {title} 
-    </button>
-  )
-}
+const Button = ({ title, handler }) => {
+  return <button onClick={handler}>{title}</button>
+};
 
-const Statistic = ({ name, val}) => {
+const Statistic = ({ stats }) => {
+  const totalFeedback = stats.good + stats.neutral + stats.bad
+
+  if (totalFeedback === 0) return <div>No feedback given</div>
+  
   return (
     <div>
-      {name} {val}
+      {Object.entries(stats).map(([key, value]) => (
+        <div>
+          {key} {value}
+        </div>
+      ))}
     </div>
   )
 }
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
 
   const increaseGood = () => {
-    const newGood = good + 1
-    const newAll = all + 1
-    setGood(newGood)
-    setAll(newAll)
-    setAverage((newGood - bad) / newAll)
-    setPositive((newGood / newAll) * 100)
-  }
-  
+    setGood(good + 1)
+  };
+
   const increaseNeutral = () => {
-    const newAll = all + 1
     setNeutral(neutral + 1)
-    setAll(newAll)
-    setPositive((good / newAll) * 100)
-  }
-  
+  };
+
   const increaseBad = () => {
-    const newBad = bad + 1
-    const newAll = all + 1
-    setBad(newBad)
-    setAll(newAll)
-    setAverage((good - newBad) / newAll)
-    setPositive((good / newAll) * 100)
+    setBad(bad + 1)
+  };
+
+  const totalFeedback = good + neutral + bad
+  const average = totalFeedback ? (good - bad) / totalFeedback : 0
+  const positive = totalFeedback ? (good / totalFeedback) * 100 : 0
+
+  const statistics = {
+    good,
+    neutral,
+    bad,
+    all: totalFeedback,
+    average: average.toFixed(2),
+    positive: positive.toFixed(2) + '%',
   }
 
   return (
     <div>
       <Title title="give feedback" />
-      <Button title="good" handler={increaseGood}/>
-      <Button title="neutral" handler={increaseNeutral}/>
-      <Button title="bad" handler={increaseBad}/>
+      <Button title="good" handler={increaseGood} />
+      <Button title="neutral" handler={increaseNeutral} />
+      <Button title="bad" handler={increaseBad} />
       <Title title="statistics" />
-      <Statistic name="good" val={good} />
-      <Statistic name="neutral" val={neutral} />
-      <Statistic name="bad" val={bad} />
-      <Statistic name="all" val={all} />
-      <Statistic name="average" val={average} />
-      <Statistic name="positive" val={positive} />
+      <Statistic stats={statistics} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
