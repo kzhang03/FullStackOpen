12b23e19c46c973e9effd53b18035d872068eeb6
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-02 23:31:37 -0400

    Updated
    Took way too long

--- Part 2/src/App.jsx
+++ Part 2/src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react'
 
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
 
-const App = (props) => {
-  const { notes } = props
+  const addNumber = (event) => {
+    event.preventDefault()
+    const newPerson = { name: newName }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
+  const nameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>        
-        {notes.map(note => <li>{note.content}</li>)}      
-      </ul>    
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={nameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => <div key={i}>{person.name}</div>)}
     </div>
   )
 }
