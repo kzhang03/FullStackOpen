63dcc34023fabc4f0c25be80f85899a664a57f1b
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-02 22:12:26 -0400

    Finished 2.1

--- Part 2/src/App.jsx
+++ Part 2/src/App.jsx
@@ -1,16 +1,48 @@
+const Header = ({ header }) => {
+  return <h1>{header}</h1>
+}
 
+const Content = ({ part }) => {
+  return (
+    part.map((section, i) => 
+      <div key={i}>{section.name} {section.id}</div>
+    )
+  )
+}
 
-const App = (props) => {
-  const { notes } = props
-
+const Course = ({ course }) => {
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>        
-        {notes.map(note => <li>{note.content}</li>)}      
-      </ul>    
+      <Header header={course.name} />
+      <Content part={course.parts} />
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
