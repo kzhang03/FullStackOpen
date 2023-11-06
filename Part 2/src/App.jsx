07b427b9fdaad023b1773f8038eb53edd51cafa6
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-05 20:46:39 -0500

    update

--- Part 2/src/App.jsx
+++ Part 2/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNumber = (event) => {
     event.preventDefault()
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber }
     const isDuplicate = persons.some(person => person.name === newPerson.name)
     if (isDuplicate) {
       alert(`${newPerson.name} is already added to phonebook`)
@@ -23,19 +24,22 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const numberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNumber}>
-        <div>
-          name: <input value={newName} onChange={nameChange}/>
-        </div>
+        <div> name: <input value={newName} onChange={nameChange}/> </div>
+        <div> number: <input value={newNumber} onChange={numberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => <div key={i}>{person.name}</div>)}
+      {persons.map((person, i) => <div key={i}>{person.name} {person.number}</div>)}
     </div>
   )
 }
