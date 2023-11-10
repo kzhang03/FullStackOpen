bf62e42d3ecf320d3c7c0ac30995a9fcc5dcacf6
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-09 22:32:09 -0500

    Update

---
+++ Part 3/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const entries = persons.length
+    const now = new Date()
+    const timestamp = now.toString()
+    const responseContent = 
+        `<div>Phonebook has info for ${entries} people</div>
+        <br />
+        <div>${timestamp}</div>`
+    response.send(responseContent)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
