42d678274658d8e4c813b31324d7b027e8403d5f
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-10 21:43:57 -0500

    Test

--- Part 3/index.js
+++ Part 3/index.js
@@ -111,6 +111,10 @@ app.post('/api/persons', (request, response) => {
     response.send(person)
 })
 
+app.get('/', (req, res) => {
+    res.send('Welcome to my Express app!');
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
