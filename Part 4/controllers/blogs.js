d39b0402aaf4cc62ff3a3c3a52e4f128acd01a14
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-14 22:49:00 -0500

    test

--- Part 4/controllers/blogs.js
+++ Part 4/controllers/blogs.js
@@ -1,6 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
+blogsRouter.get('/', (request, response) => {
+    response.send("Hello!")
+})
+
 blogsRouter.get('/api/blogs', (request, response) => {
     Blog.find({}).then(blogs => {
         response.json(blogs)
