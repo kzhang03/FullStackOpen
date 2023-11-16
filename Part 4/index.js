3a92200999669c4d6310ba350eb0d546edfb5a41
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-15 21:40:34 -0500

    Update 2

--- Part 4/index.js
+++ Part 4/index.js
@@ -1,32 +1,27 @@
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const Blog = require('./models/blog')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.set('strictQuery', false)
 
 mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error.message)
+        process.exit(1)
+    })
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = 3003
 app.listen(PORT, () => {
