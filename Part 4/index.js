354ef7878c647f48144b051260dadf48b924f109
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-15 21:56:00 -0500

    Maybe Final

--- Part 4/index.js
+++ Part 4/index.js
@@ -1,29 +1,8 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app')
 
-mongoose.set('strictQuery', false)
-
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB', error.message)
-        process.exit(1)
-    })
-
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
