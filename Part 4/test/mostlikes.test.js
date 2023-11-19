13d42b5b28ab627ddb57ffaae84b33d208349627
kzhang03 <ker.zhang@mail.utoronto.ca>
2023-11-18 22:23:50 -0500

    update

---
+++ Part 4/test/mostlikes.test.js
@@ -0,0 +1,28 @@
+const listHelper = require('../utils/testing')
+
+describe('most likes', () => {
+    const listBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+          likes: 5,
+          __v: 0
+        },
+        {
+            title: 'Test',
+            author: 'Test 1',
+            likes: 10
+        }
+    ]
+
+    test('when list has only one blog, equals the likes of that', () => {
+      const result = listHelper.favoriteBlog(listBlog)
+      expect(result).toEqual({
+        title: 'Test',
+        author: 'Test 1',
+        likes: 10
+    })
+    })
+})
--- Part 4/utils/testing.js
+++ Part 4/utils/testing.js
@@ -6,7 +6,26 @@ const TestLikes = (blog) => {
     return blog[0].likes
 }
 
+const favoriteBlog = (blogs) => {
+    let bestBlog = {
+        title: "",
+        author: "",
+        likes: -1
+    }
+    for (const blog of blogs) {
+        if (blog.likes >= bestBlog.likes) {
+            bestBlog = { 
+                title: blog.title, 
+                author: blog.author, 
+                likes: blog.likes 
+            }
+        }
+    }
+    return bestBlog
+}
+
 module.exports = {
     dummy,
     TestLikes,
+    favoriteBlog,
 }
